3726932ebbb83703edd486a4c8fb29999668412e
DONGWON KIM <lunacircle4@gmail.com>
2019-05-03 11:30:56 +0900

    Implement using Jquery ajax

--- vjango/views.py
+++ vjango/views.py
@@ -4,8 +4,5 @@ from django.http import JsonResponse
 def index(request):
     return render(request, "index.html", {'days' : [1, 2, 3]})
 
-def index(request):
-    return render(request, "index.html", {'days' : [1, 2, 3]})
-
 def indexTest(request):
     return JsonResponse({'days' : [1, 2, 3]})
